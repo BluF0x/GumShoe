0ffc5731b1b09c8dc70e8bd5836276208971f8ae
BluF0x <josue.elias.f@gmail.com>
2025-04-01 10:41:14 -0300

    Added CMake

---
+++ CMakeFiles/MySDL3Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySDL3Project.dir/link.d"
+  "CMakeFiles/MySDL3Project.dir/main.cpp.o"
+  "CMakeFiles/MySDL3Project.dir/main.cpp.o.d"
+  "MySDL3Project"
+  "MySDL3Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySDL3Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
